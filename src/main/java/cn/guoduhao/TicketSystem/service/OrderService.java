3993f7e80506b97a3a78c54598557d3527ac8eb1
frank <i@nyan.im>
2018-07-10 14:51:19 +0800

    完成订单查询

--- src/main/java/cn/guoduhao/TicketSystem/service/OrderService.java
+++ src/main/java/cn/guoduhao/TicketSystem/service/OrderService.java
@@ -7,12 +7,12 @@ import cn.guoduhao.TicketSystem.repository.TrainRepository;
 import cn.guoduhao.TicketSystem.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
 
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @EnableJms
@@ -26,6 +26,9 @@ public class OrderService {
 
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     public int placeOrder(String userId, Integer trainId){
         ObjectMapper mapper = new ObjectMapper();
@@ -56,5 +59,22 @@ public class OrderService {
     private int pushQueue(String orderJson){
         this.jmsMessagingTemplate.convertAndSend("orders", orderJson);
         return 0;
+    }
+
+    public List<Ticket> readOrdersFromRedis(String userId){
+        Set<String> keys = this.stringRedisTemplate.keys("*"+userId);
+        List<String> jsonList = this.stringRedisTemplate.opsForValue().multiGet(keys);
+
+        List<Ticket> ticketList = new ArrayList<>();
+        for(int i=0;i<jsonList.size();i++){
+            ObjectMapper mapper = new ObjectMapper();
+            try{
+                Ticket ticket = mapper.readValue(jsonList.get(i), Ticket.class);
+                ticketList.add(ticket);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return ticketList;
     }
 }
