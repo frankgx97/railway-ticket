3b69ea6d3e93808f404c6c167a69bb58a4d05849
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-07-11 15:57:56 +0800

    分段购票 北京-上海 线 整合完毕。

--- src/main/java/cn/guoduhao/TicketSystem/service/ticket/TicketServiceImpl.java
+++ src/main/java/cn/guoduhao/TicketSystem/service/ticket/TicketServiceImpl.java
@@ -36,28 +36,30 @@ public class TicketServiceImpl implements TicketService{
 
     @Override
     public Integer buyTicket_BJ_SH(Ticket newTicket){
+        //在剩余的半程票中是否已经成功购票
         boolean isSuccess = buyRemanentTicket_BJ_SH(newTicket);
         if(isSuccess){
-            return 1;
+            return 1;//若已成功购票 则返回1
         }
-        else{
+        else{//否则，从全程票中查看是否有空闲票
             //ToDo 需要更改 进入NoSQL中查询含有顾客上车和下车站的trainNo, 再根据TrainNo检索
             List<Train> trains =
                 trainRepository.findByDepartStationAndDestinationStation("北京","上海");
             Integer remanentTickets = trains.get(0).seatsTotal - trains.get(0).seatsSold;
-            if(remanentTickets > 0){
+            if(remanentTickets > 0){//若全程票中有空闲票
                 String startStation = newTicket.departStation;
                 String arriveStation =newTicket.destinationStation;
-                String defaultStation = createStations("北京","上海");
+                //创建新的stations字段
+                String defaultStation = createStations_BJ_SH("北京","上海");
                 String newStations = modifyStations(startStation,arriveStation,defaultStation);
-                trains.get(0).seatsSold += 1;
+                trains.get(0).seatsSold += 1;//全程票售出一张(全程半程均可)
                 newTicket.stations = newStations;
-                ticketRepository.save(newTicket);
-                trainRepository.save(trains.get(0));
-                return 1;
+                ticketRepository.save(newTicket);//更新新买的票至Ticket表
+                trainRepository.save(trains.get(0));//更新Train表
+                return 1;//返回成功
             }
             else{
-                return 2;
+                return 2;//表示没有合适的票
             }
         }
     }
@@ -65,39 +67,43 @@ public class TicketServiceImpl implements TicketService{
     //ToDo JB_SH推广时需要修改
     @Override
     public boolean buyRemanentTicket_BJ_SH(Ticket newTicket){
-        //读出传入数据的起始站和目的站
+        //读出乘客的起始站和目的站
         String startStation = newTicket.departStation;
         String arriveStation = newTicket.destinationStation;
         //ToDo JB_SH推广时需要修改
+        //在Ticket表中搜索相应段全部为0的票务信息
         List<Ticket> targetTickets = searchRemanentTicket_BJ_SH(startStation,arriveStation);
         //搜索符合条件的剩余票
         if(!targetTickets.isEmpty()){
             String targetStations = targetTickets.get(0).stations;
+            //新建String stations
             String newStations = modifyStations(startStation,arriveStation,targetStations);
-            if (newStations.equals(targetStations)){ //若stations未发生改动
-                return false; // 则返回 表示修改失败 购票失败 剩余的票中已经没有满足需求的分段票了
+            if (newStations.equals(targetStations)){ //若stations未发生改动(表示没有修改成功)
+                return false; // 则返回false 表示修改失败 购票失败 剩余的票中已经没有满足需求的分段票了
             }
             else{
                 //同时更新新票和已经购入票的stations字段
                 newTicket.stations = newStations;
                 targetTickets.get(0).stations = newStations;
                 //ToDo:改
+                //当选购票后，票的stations字段全部变为"1" 说明已经凑出了一张全程票
                 if (newStations.equals(modifyStations("北京","上海",""))){
+                    //找到原来半程票对应的trainNo
                     Optional<Train> train = trainRepository.findOneByTrainNo(targetTickets.get(0).trainNo);
                     if(train.isPresent()){
-                        train.get().seatsSold += 1;
-                        trainRepository.save(train.get());
+                        train.get().seatsSold += 1; //trainNo对应的seatsSold + 1
+                        trainRepository.save(train.get()); //更新Train表，改变剩余票数
                     }
-                    return true; //表示需要在上层函数中从总票数里卖出一张
+                    return true; //表示购票成功
                 }
 
                 //将这两张票在数据库中更新
                 ticketRepository.save(newTicket);
                 ticketRepository.save(targetTickets.get(0));
-                return true; //返回 1购票成功 表示此次购票可以在剩余的票中解决
+                return true; //返回 true 购票成功 表示此次购票可以在剩余的票中解决
             }
         }
-        return false; // 返回 0购票失败 剩余的票中已经没有满足需求的分段票了
+        return false; // 返回 false 购票失败 剩余的票中已经没有满足需求的分段票了
     }
 
     //ToDo 添加北京至上海的分段查找功能 如果铁路线路过多需要使用数据库存储。具体实现逻辑在石墨文档
@@ -147,7 +153,7 @@ public class TicketServiceImpl implements TicketService{
         return modifyString(departNum,destinationNum,stations);
     }
 
-    //分段式构建
+    //分段式构建 示例:
     //北京-石家庄-邯郸-郑州
     //    0      0    0
     //以下是用于分段购票的实现函数
@@ -177,12 +183,14 @@ public class TicketServiceImpl implements TicketService{
         return Arrays.toString(newStations).replaceAll("[\\[\\]\\s,]", "");
     }
 
-    private String createStations(String departStation,String destinationaStation){
+    //生成一个北京-上海的"0000000000"字段
+    private String createStations_BJ_SH(String departStation,String destinationaStation){
         Integer departNum = StringToStationNum_BJ_SH(departStation);
         Integer destinationNum = StringToStationNum_BJ_SH(destinationaStation);
         return createStations(departNum,destinationNum);
     }
 
+    //输入站号，自动生成相应位数的stations字段
     private String createStations(Integer departNum,Integer destinationNum){
         String defaultStation = "";
         for(int i = departNum; i < destinationNum ; i++){
