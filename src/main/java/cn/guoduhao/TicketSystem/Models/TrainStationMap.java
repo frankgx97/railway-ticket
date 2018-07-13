eb65e84dc9b40ec983c0c183cc40d4af1c7c7829
frank <i@nyan.im>
2018-07-13 18:21:12 +0800

    去除MongoDB模型中的entity注解

--- src/main/java/cn/guoduhao/TicketSystem/Models/TrainStationMap.java
+++ src/main/java/cn/guoduhao/TicketSystem/Models/TrainStationMap.java
@@ -6,8 +6,6 @@ import javax.persistence.*;
 import java.util.List;
 
 
-@Entity
-@Table
 public class TrainStationMap {
     @Id
     @GeneratedValue(generator="system-uuid", strategy= GenerationType.AUTO)
