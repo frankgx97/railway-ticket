d08f3302472e455cc010d4b594b143291c387a04
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-07-13 16:22:29 +0800

    新开TrainMongoRepository

---
+++ src/main/java/cn/guoduhao/TicketSystem/repository/MongoDbRepositories/TrainMongoRepository.java
@@ -0,0 +1,10 @@
+package cn.guoduhao.TicketSystem.repository.MongoDbRepositories;
+
+
+import cn.guoduhao.TicketSystem.Models.Ticket;
+import cn.guoduhao.TicketSystem.Models.Train;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TrainMongoRepository extends MongoRepository{
+
+}
