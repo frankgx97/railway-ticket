6f4a4bd59e031e6063929bc02afb9c7674daa999
frank <i@nyan.im>
2018-07-16 00:05:32 +0800

    完成历史订单查询

--- src/main/java/cn/guoduhao/TicketSystem/Controllers/HomeController.java
+++ src/main/java/cn/guoduhao/TicketSystem/Controllers/HomeController.java
@@ -2,6 +2,7 @@ package cn.guoduhao.TicketSystem.Controllers;
 
 import cn.guoduhao.TicketSystem.Models.CurrentUser;
 import cn.guoduhao.TicketSystem.Models.Ticket;
+import cn.guoduhao.TicketSystem.repository.MongoDbRepositories.TicketMongoRepository;
 import cn.guoduhao.TicketSystem.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,11 +18,22 @@ public class HomeController {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    TicketMongoRepository ticketMongoRepository;
+
     @RequestMapping("/home")
     public String home(Map<String, List<Ticket>> map){
         CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         List<Ticket> ticketList = this.orderService.readOrdersFromRedis(currentUser.getId());
         map.put("ticketList",ticketList);
         return "home";
+    }
+
+    @RequestMapping("/history")
+    public String history(Map<String, List<Ticket>> map){
+        CurrentUser currentUser = (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Ticket> ticketList = this.ticketMongoRepository.findByUserId(currentUser.getId());
+        map.put("ticketList", ticketList);
+        return "history";
     }
 }
--- src/main/java/cn/guoduhao/TicketSystem/repository/MongoDbRepositories/TicketMongoRepository.java
+++ src/main/java/cn/guoduhao/TicketSystem/repository/MongoDbRepositories/TicketMongoRepository.java
@@ -3,5 +3,8 @@ package cn.guoduhao.TicketSystem.repository.MongoDbRepositories;
 import cn.guoduhao.TicketSystem.Models.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface TicketMongoRepository extends MongoRepository<Ticket, String> {
+    List<Ticket> findByUserId(String userId);
 }
